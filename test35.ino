563a999050dd0c15be2665b49c63a71b0c0913e7
Charles D <depalman17@gmail.com>
2019-05-07 07:42:47 -0400

    fixed bugs and namespace collision in test program

--- test35.ino
+++ test35.ino
@@ -5,10 +5,8 @@
 SX1509 io0; SX1509 io1; SX1509 io2;
 void setup() 
 {
-	if (!io0.begin(0x3E) || !io1.begin(0x3F) || !io2.begin(0x70))
-	{
-		while ( 1 ); // If we fail to communicate, loop forever.
-	}
+	// if we fail to communicate, halt
+	if (!io0.begin(0x3E) || !io1.begin(0x3F) || !io2.begin(0x70)) while ( 1 );
   
   	// set all pins to output
   	for ( unsigned int i = 0; i < 16; i += 1 )
@@ -19,22 +17,22 @@ void setup()
 	}
 }
 
-uint8_t map[ 35 ];
+uint8_t dmap[ 35 ];
 void mapdisplay()
 {
   	for ( unsigned int i = 0; i < 16; i += 1 )
   	{
-  		io0.digitalWrite( i, map[ i ] 			   );
-		io1.digitalWrite( i, map[ i + 16 ] 		   );
-		io2.digitalWrite( i, i >= 35 ? LOW : map[ i + 32 ] );
+  		io0.digitalWrite( i, dmap[ i ] 			  );
+		io1.digitalWrite( i, dmap[ i + 16 ] 		  );
+		io2.digitalWrite( i, i > 3 ? LOW : dmap[ i + 32 ] );
 	}
 }
 
-unsigned int itr = 0;
+unsigned int iter = 0;
 void loop() 
 {
-	memset( map, 0x00, 35 ); // clear display
-	map[ iter % 35 ] = HIGH;
+	memset( dmap, 0x00, 35 ); // clear display
+	dmap[ iter % 35 ] = HIGH;
 	mapdisplay();
 	delay( 50 );
 	iter++;
